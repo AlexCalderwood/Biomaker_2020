e5019226109b91754014c206bc81b693dd4bc82f
AlexCalderwood <alex.calderwood@cantab.net>
2021-02-24 22:16:45 +0000

    dashboard timeseries plots added

--- dashboard/server.R
+++ dashboard/server.R
@@ -2,27 +2,107 @@
 here::i_am('dashboard/server.R')
 library(here)
 library(data.table)
-library(DT)
+library(stringr)
+library(DT) # R interface to JavaScript "DataTables" libarary
 
-statsPath <- here('plotting_and_stats', 
-                  'example_output_model_A')
+# path to stats summary
+statsPath <- paste0('www/example_output')
+# path to statistics timeseries plot
+plotPath <- paste0(statsPath, '/plots/')
 
-summary_table <- fread(paste0(statsPath, '/summary_table.csv'))
 
 red = '#D22D5C'
 green = '#2CD16B'
 
 # server ---------------------------------------------------
 
-server <- function(input, output) {
+server <- function(input, output, session) {
+
+  # summary results table --------------------------------------------------
+  # format table content
   
+  summary_table <- fread(paste0(statsPath, '/summary_table.csv'))
+  print(summary_table)
+  names(summary_table) <- str_replace(names(summary_table), 
+                                      pattern='-', 
+                                      replacement=' vs ')
   summary_table[summary_table==TRUE] <- 'different'
   summary_table[summary_table==FALSE] <- 'same'
-  # format table
   
-  output$summary_table <- DT::renderDataTable({datatable(summary_table) %>% 
-      formatStyle(2:ncol(summary_table),
-                  backgroundColor=DT::styleEqual(c('different', 'same'), 
-                                                 c(red, green)))
+  # control table styling
+  output$summary_table <- DT::renderDataTable(
+    {
+      print('rendering table...')
+      datatable(summary_table,
+               rownames=F,
+               options=list(
+                 columnDefs=list(list(className='dt-center', targets='_all')))
+               ) %>% 
+      DT::formatStyle(2:ncol(summary_table),
+                  backgroundColor=DT::styleEqual(c('different', 'same'), c(red, green)))
     })
+  
+  # timeseries plots -------------------------------------------------------
+  # for quick convenience, just open as image. Want eventually to be able to 
+  # at least open as object, so dynamic sizing works etc.
+
+  # get the paths for the plots for the reactive metric
+  data_plot_path <- reactive({
+    paste0(plotPath, '/', input$timeseries_metric, '/data.plot.png')
+  })
+  plant_fit_plot_path <- reactive({
+    paste0(plotPath, '/', input$timeseries_metric, '/plant.fit.plot.png')
+  })
+  group_fit_plot_path <- reactive({
+    paste0(plotPath, '/', input$timeseries_metric, '/grp.fit.plot.png')
+  })
+  difference_plot_path <- reactive({
+    paste0(plotPath, '/', input$timeseries_metric, '/grp.diff.plot.png')
+  })
+  
+  # render the timeseries plots. Use renderImage, as
+  # renderPlot is pretty slow.
+  output$data_plot <- renderImage(
+    {
+     # use w for width and height, as height doesn't change, 
+     # and are the same anyway
+     w <- session$clientData$output_data_plot_width 
+     #readRDS(data_plot_path())
+      list(src=data_plot_path(),
+           width=w,
+           height=w)
+    }, deleteFile=F
+  )
+  output$plant_fit_plot <- renderImage(
+    {
+      w <- session$clientData$output_data_plot_width 
+      #readRDS(plant_fit_plot_path())
+      list(src=plant_fit_plot_path(),
+           width=w,
+           height=w)
+    }, deleteFile=F
+  )
+  output$group_fit_plot <- renderImage(
+    {
+      w <- session$clientData$output_data_plot_width 
+      #readRDS(group_fit_plot_path())
+      list(src=group_fit_plot_path(),
+           width=w,
+           height=w)
+    }, deleteFile=F
+  )
+  output$difference_plot <- renderImage(
+    {
+      w <- session$clientData$output_data_plot_width 
+      #readRDS(difference_plot_path())
+      list(src=difference_plot_path(),
+           width=w,
+           height=w)
+    }, deleteFile=F
+  )
 }
+
+  
+  
+  
+  
--- dashboard/ui.R
+++ dashboard/ui.R
@@ -16,11 +16,52 @@ summary_panel <- tabPanel("Summary",
                           titlePanel("Results Summary"),
                           fluidRow(
                             column(12, 
-                                   dataTableOutput('summary_table')
+                                   DT::dataTableOutput('summary_table')
                                    )),
-                          p("[end of results]"))
+                          p(em("*different indicates that a period of different response to the cabinet exists between these groups"))
+                          )
+
+# # page 2 - Timeseries graphs -------------------------
+sidebar_content <- sidebarPanel(
+  selectInput(
+    'timeseries_metric',
+    label = 'Metric:',
+    choices = summary_table$metric),
+  tags$p(tags$b('Raw Data:'), 'shows the data extracted from the imaging'),
+  tags$p(tags$b('Individual Fit:'), 'shows the fitted function for each individual plant'),
+  tags$p(tags$b('Group Fit:'), 'shows the modelled function ±2 s.e. for each plant group'),
+  tags$p(tags$b('Group Difference'), 'shows the mean ±2 s.e. for the estimated differences in 
+         response between the groups'),
+  tags$p("Changes in the metrics over time were modelled as hierarchical spline models. So the value for each 
+  individual plant is modelled as the sum of the effect of the group it's in, and it's deviation
+  from the group."),
+  tags$p("Coefficients are estimated using ", tags$em("fREML (fast Resticted Maximum Likelihood)"),
+         'using the ', tags$em('mgcv'), ' package in R.'),
+  tags$p("Hierarchical models can be unstable due to the inherantly correlated coefficients - be sure to
+  check that the divergence of individual plants from the group means are not overfitting, and 
+         compensating for a poorly modelled group effect!")
+  )
+
+main_content <- mainPanel(
+  fluidRow(column(width=6, tags$h3('Raw Data', style='text-align:center;'), 
+                  imageOutput('data_plot', height='100%')), 
+           column(width=6, tags$h3('Individual Fit', style='text-align:center;'), 
+                  imageOutput('plant_fit_plot',  height='100%'))),
+  fluidRow(column(width=6, tags$h3('Group Fit', style='text-align:center;'), 
+                  imageOutput('group_fit_plot',  height='100%')),
+           column(width=6, tags$h3('Group Difference', style='text-align:center;'), 
+                  imageOutput('difference_plot',  height='100%')))
+)
+
+timeseries_panel <- tabPanel("Timeseries Results",
+                             titlePanel("Timeseries Results"),
+                             sidebarLayout(sidebar_content,
+                                           main_content)
+                             )
+                             
 
 
 # define user interface as combo of above ------------
-ui <- navbarPage('[Nav bar title]', 
-                 summary_panel)
+ui <- navbarPage('Stressful cabinet', 
+                 summary_panel,
+                 timeseries_panel)
