d05825e1203e9b47abb5f8f0884ddcfacb40f942
biomaker_pi <ms2745@cam.ac.uk>
2021-03-24 20:37:09 +0000

    Fixed new lepton class

--- control_software/image_capture/lepton_preview.py
+++ control_software/image_capture/lepton_preview.py
@@ -1,18 +1,14 @@
-from lepton_utils import preview_lepton
+from lepton_utils import LeptonCamera
 
 
 def run():
-    preview_lepton()
+    #raw_dir = "~/Biomaker/Biomaker_2020/control_software/image_capture/raw_imgs"
+    #img_dir = "~/Biomaker/Biomaker_2020/control_software/image_capture/imgs"
+    raw_dir = "raw_imgs/"
+    img_dir = "imgs/"
+    lep = LeptonCamera(raw_dir, img_dir)
+    lep.preview()
 
 
 if __name__ == "__main__":
     run()
-
-
-"""
-plt.imsave("imgs/frame_000000.png", raw_image, cmap='gray')
-print("Image saved!")
-
-plt.imshow(raw_image,cmap = 'gray')
-plt.show()
-"""
--- control_software/image_capture/lepton_utils.py
+++ control_software/image_capture/lepton_utils.py
@@ -13,56 +13,71 @@ def prepend_byte(loc, raw):
 
 
 class LeptonCamera:
-
-    lepton_dir = "~/RaspberryPi/lepton_data_collector/./lepton_data_collector
-
-    #"~/Biomaker/Biomaker_2020/image_capture/raw_imgs/"
-
+    """Class to access Lepton Camera."""
+    
+    lepton_exe = "~/RaspberryPi/lepton_data_collector/./lepton_data_collector"
+    
 
     def __init__(self, raw_dir, img_dir):
         """ Constructor
         """
-        self.raw_dir = raw_dir
-        self.img_dir = img_dir
+        self.set_raw_dir(raw_dir)
+        self.set_img_dir(img_dir)
     
     
     def set_raw_dir(self, raw_dir):
+        raw_dir = str(raw_dir)
+        if raw_dir[-1] != "/":
+            raw_dir += "/"
         self.raw_dir = raw_dir
 
     
     def set_img_dir(self, img_dir):
+        img_dir = str(img_dir)
+        if img_dir[-1] != "/":
+            img_dir += "/"
         self.img_dir = img_dir
 
 
-    def capture_image(self, raw_dir=self.raw_dir, raw_name="frame", images=1):
-        """ Instructs lepton camera to take a number of images, and saves them in raw directory as "frame_xxxxxx.GRAY"
+    def generate_raw_img(self, raw_dir=None, raw_name="frame_", images=1):
+        """ Instructs lepton camera to take a number of images,
+            and saves them in raw directory as "<raw_name>xxxxxx.GRAY"
         """
-        os.system(lepton_dir + f"-3 -c {images} -o " + raw_dir + raw_name)
+        if not raw_dir:
+            raw_dir = self.raw_dir
+
+        os.system(self.lepton_exe + f" -3 -c {images} -o " + raw_dir + raw_name)
 
 
-    def convert_raw_img(self, raw_dir=self.raw_dir, raw_name):
+    def convert_raw_img(self, raw_dir=None, raw_img_name="frame_000000.gray"):
         """ Converts raw .GRAY file to a (120x160) np array of 'int16's
         """
-        raw_image = np.fromfile(raw_dir+raw_name, dtype='>i2')  # Read as int16, BIG ENDIAN
+        if not raw_dir:
+            raw_dir = self.raw_dir
+
+        raw_image = np.fromfile(raw_dir+raw_img_name, dtype='>i2')  # Read as int16, BIG ENDIAN
         raw_image.shape = (120,160)
         return raw_image
 
     
-    def get_image_array(self, raw_dir=self.raw_dir, raw_name="frame"):
-        """ Obtains a new image array from the attached lepton camera, and converts it to np array
+    def generate_img_array(self, raw_dir=None, raw_name="frame_"):
+        """ Obtains a new image from attached lepton camera, and converts it to np array
         """
-        self.capture_image(raw_dir, raw_name)
+        if not raw_dir:
+            raw_dir = self.raw_dir
+        
+        self.generate_raw_img(raw_dir, raw_name)
         return self.convert_raw_img(raw_dir, str(raw_name) + "000000.gray")
 
 
     def preview(self):
 
         fig, ax = plt.subplots(1,1)
-        image = self.get_image_array(raw_name="frame_1")
+        image = self.generate_img_array(raw_name="frame_1")
         im = ax.imshow(image, cmap='gray')
 
         while True:
-            image = self.get_image_array(raw_name="frame_1")  # frame_1 used for previews, frame_0 used for data
+            image = self.generate_img_array(raw_name="frame_1")  # frame_1 used for previews, frame_0 used for data
             im.set_data(image)
             fig.canvas.draw_idle()
             plt.pause(1)
